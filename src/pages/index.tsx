a7a8c080dabd24303062f2f48afc6322f61eccc6
Natsusaka <zz5120937@gmail.com>
2023-11-27 23:58:32 +0800

    feat/mount masthead component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Masthead from '../components/masthead'
+import AboutUs from '@/components/aboutus'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,8 +10,14 @@ export default function Home() {
   return (
 
     <div className="">
+      <Head>
+        <title>Natsusaka</title>
+        <meta name="description" content="Next.js pratice" />
+        <link rel="icon" href="/assets/logo.png" />
+      </Head>
       <div className='font-bold text-9xl'></div>
-      <Masthead></Masthead>
+      <Masthead />
+      <AboutUs />
     </div>
   )
 }
